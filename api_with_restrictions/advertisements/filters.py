fdf7f4e19453b51db19390049fbf4dd7c12550e3
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-02-08 20:55:14 +0500

    It's alive!

--- api_with_restrictions/advertisements/filters.py
+++ api_with_restrictions/advertisements/filters.py
@@ -1,15 +1,16 @@
 from django_filters import rest_framework as filters
-from django_filters import DateFromToRangeFilter
+
 
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-    created_at = DateFromToRangeFilter()
+    created_at = filters.DateFromToRangeFilter(field_name='created_at')
+    creator = filters.CharFilter()
 
     # TODO: задайте требуемые фильтры
 
     class Meta:
         model = Advertisement
-        fields = ['created_at', 'creator']
+        fields = ['creator', 'created_at']
--- api_with_restrictions/advertisements/views.py
+++ api_with_restrictions/advertisements/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
@@ -11,6 +11,7 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = (DjangoFilterBackend, )
     filterset_class = AdvertisementFilter
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
