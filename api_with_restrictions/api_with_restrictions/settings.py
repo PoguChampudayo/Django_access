f63abf7a0089a612225847b36ca8df182c1c1834
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-02-09 16:39:38 +0500

    added throttling

--- api_with_restrictions/api_with_restrictions/settings.py
+++ api_with_restrictions/api_with_restrictions/settings.py
@@ -81,6 +81,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '20/minute',
+        'anon': '10/minute'
+    }
     
 }
 
